0c8e4bce565de039005eaafbcb74e095a2610a34
dwalke22 <dwalke22@cs3280-vm13.uwg.westga.edu>
2020-10-28 11:20:31 -0400

    added functionality to takee arguments from command

--- main.py
+++ main.py
@@ -4,8 +4,21 @@ Port scanner that attempts to make a
 TCP connection
 """
 import multiprocessing
+import sys
 
 __author__ = "David Walker"
 __version__ = "Fall 2020"
 
+def main():
+    """The Main entry point for the application"""
+    ip_address = sys.argv[1]
+    start_port = sys.argv[2]
+    print(ip_address)
+    print(start_port)
+    if len(sys.argv) == 4:
+        end_port = sys.argv[3]
+        print(end_port)
+
+
 if __name__ == "__main__":
+    main()
