1ec0eb7e3e70d47c430e91fe0793347f4134068a
dwalke22 <dwalke22@cs3280-vm03.uwg.westga.edu>
2020-10-27 20:25:31 -0400

    Started basif format for main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/user/bin/env python3
+"""
+Port scanner that attempts to make a
+TCP connection
+"""
+import multiprocessing
+
+__author__ = "David Walker"
+__version__ = "Fall 2020"
+
+if __name__ == "__main__":
