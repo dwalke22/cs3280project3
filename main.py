4b4c27dc05766c912bec44c62694beae86caff5e
dwalke22 <dwalke22@cs3280-vm03.uwg.westga.edu>
2020-11-02 15:29:22 -0500

    moved scann function to utils

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 Port scanner that attempts to make a
 TCP connection
 """
+import argparse
 import sys
 from socket import AF_INET, SOCK_STREAM, socket
 import utils
@@ -11,19 +12,9 @@ __author__ = "David Walker"
 __version__ = "Fall 2020"
 
 if __name__ == "__main__":
+    
     IP_ADDRESS = sys.argv[1]
     if not utils.verify_ipv4(IP_ADDRESS):
         print("Not Valid IP")
     START = int(sys.argv[2])
-    if len(sys.argv) == 4:
-        END = int(sys.argv[3])
-        for i in range(START, END):
-            s = socket(AF_INET, SOCK_STREAM)
-            conn = s.connect_ex((IP_ADDRESS, i))
-            print("Port " + str(i) + " " + str(conn))
-            s.close()
-    else:
-        for i in range(START,):
-            s = socket(AF_INET, SOCK_STREAM)
-            conn = s.connect_ex((IP_ADDRESS, i))
-            print("Port " + str(i) + " " + str(conn))
+    END = int(sys.argv[3])
--- utils.py
+++ utils.py
@@ -3,10 +3,20 @@
 Provides utility methods for scanning TCP connections
 """
 import re
-import socket
+from socket import AF_INET, SOCK_STREAM, socket
 
 def verify_ipv4(ip_address):
     """Verifies that the ip is a valid IPv4 address"""
     ip_regex = re.compile(r"""^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}
             (?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$""", re.X)
     return ip_regex.match(ip_address)
+
+def scan(ip_address, start_port, end_port):
+    """Scans all ports"""
+    ports = {}
+    for i in range(start_port, end_port):
+        scanner = socket(AF_INET, SOCK_STREAM)
+        conn = scanner.connect_ex((ip_address, i))
+        ports[str(i)] = conn
+        scanner.close()
+    return ports
